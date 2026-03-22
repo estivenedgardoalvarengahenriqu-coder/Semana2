29897cd792acd1936e89c3d19cb23f35a5df6b67
estivenedgardoalvarengahenriqu <estivenedgardoalvarengahenriqu@gmail.com>
2026-03-21 20:02:53 -0600

    Este es el sexto ejercicio

---
+++ python/Ejercicio 6.py
@@ -0,0 +1,8 @@
+nombre_completo = input("Escribe tu nombre completo: ")
+
+# Separamos por espacios
+palabras = nombre_completo.split()
+
+# Mostramos cada palabra en una línea
+for palabra in palabras:
+    print(palabra)
