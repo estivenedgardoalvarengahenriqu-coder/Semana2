f01b9acf15e3c9bc14a81ebc6c448fee500c0cdb
estivenedgardoalvarengahenriqu <estivenedgardoalvarengahenriqu@gmail.com>
2026-03-21 20:00:19 -0600

    Este es el primer ejercicio

---
+++ python/Ejercicio 1.py
@@ -0,0 +1,7 @@
+# programa que muestra nombre en mayúsculas, minúsculas y su longitud
+
+nombre = input("Ingrese su nombre completo: ")
+
+print("Nombre en mayúsculas:", nombre.upper())
+print("Nombre en minúsculas:", nombre.lower())
+print("Cantidad de caracteres:", len(nombre))
