fe4ddf3156651842500d556248b36bd4cb493f31
estivenedgardoalvarengahenriqu <estivenedgardoalvarengahenriqu@gmail.com>
2026-04-09 22:15:03 -0600

    usar la cadena any time y anytime reemplazarlo por always y convertir lo demas en mayusculas

---
+++ Parcial 02 Fundamentos de programación/Parcial 02 Ejercicio 9.py
@@ -0,0 +1,17 @@
+##Ejercicio 9
+##1. Toma la cadena "Any time y Anytime".
+##2. Reemplaza todas las apariciones de "Any time" (con espacio) por "Always".
+##3. Convierte todo el texto resultante a mayusculas.
+
+
+## Tomar la cadena "Any time y Anytime"
+texto = "Any time y Anytime"
+print(texto)
+
+## Reemplazar "Any time" (con espacio) por "Always"
+texto_reemplazado = texto.replace("Any time", "Always")
+print(texto_reemplazado)
+
+## Convertir todo el texto a mayúsculas
+texto_mayusculas = texto_reemplazado.upper()
+print(texto_mayusculas)
