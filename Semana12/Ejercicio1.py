481b3cc4c1bc314fd8eeb316ffdc9544d10f4cc0
estivenedgardoalvarengahenriqu <estivenedgardoalvarengahenriqu@gmail.com>
2026-04-15 17:18:09 -0600

    Este es el primer ejercicio

---
+++ Semana12/Ejercicio1.py
@@ -0,0 +1,11 @@
+##Ejercicio 1:
+##Pide un número al usuario e indica si es positivo, negativo o cero usando if, elif y else.
+
+numero = int(input("Ingresa un número: "))
+
+if numero > 0:
+    print("El número es positivo")
+elif numero < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
