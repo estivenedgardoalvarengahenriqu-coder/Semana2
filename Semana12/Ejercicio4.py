fa98ba78d5e08180b05f61794cb2d90e7f5a8d9c
estivenedgardoalvarengahenriqu <estivenedgardoalvarengahenriqu@gmail.com>
2026-04-15 17:19:43 -0600

    Este es el cuarto ejercicio

---
+++ Semana12/Ejercicio4.py
@@ -0,0 +1,10 @@
+##Ejercicio 4:
+##Pide un número y determina si es par o impar.
+
+
+numero = int(input("Ingresa un número entero: "))
+
+if numero % 2 == 0:
+    print(f"{numero} es par")
+else:
+    print(f"{numero} es impar")
